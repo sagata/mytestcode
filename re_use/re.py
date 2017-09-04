64f6b7cfea782f5574f8d881279deaedd69c7e2b
sagata <15017527027@163.com>
2017-09-05 00:30:29 +0800

    123

---
+++ re_use/re.py
@@ -0,0 +1,14 @@
+#coding=utf8
+import re
+
+#re.match 尝试从字符串的开始匹配一个模式，如：下面的例子匹配第一个单词。 
+def match_test():
+	text = "JGood is a handsome boy, he is cool, clever, and so on..."
+	m = re.match(r"(\w+)\s", text)
+	if m:
+		print m.group(0), '\n', m.group(1)
+	else:
+		print 'not match'  
+	m.group(1)
+
+match_test()
--- test.py
+++
@@ -1,5 +0,0 @@
-print 'test'
-print 'test2'
-import os 
-if os.path.exists(r'E:\data2.txt'):
-	print 'tes'
